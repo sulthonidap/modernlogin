2f181aa950e53dc3fe89215a3fed84738e4d554b
sulthonidap <sulthonidap@gmail.com>
2023-04-11 09:39:45 +0700

    first commit

---
+++ app/src/main/java/com/example/loginbox/ScreenThre.kt
@@ -0,0 +1,11 @@
+package com.example.loginbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenThre : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_thre)
+    }
+}
